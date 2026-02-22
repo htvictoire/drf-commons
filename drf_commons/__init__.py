96c7ba9a8b06acd116de9f55b10b4aee62fd8eb4
Victoire HABAMUNGU <victoirehabamungut@gmail.com>
2026-02-22 01:17:00 -0500

    workflows

--- drf_commons/__init__.py
+++ drf_commons/__init__.py
@@ -4,4 +4,4 @@ Django REST Framework Common Utilities.
 A collection of reusable utilities and components for Django REST Framework applications.
 """
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
